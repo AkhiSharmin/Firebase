06da6aaad49d7131f5765e182b07ee8f09d56864
AkhiSharmin <akhisharmin100@gmail.com>
2025-04-14 19:40:09 +0600

    first commit

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,21 @@
+//do not store config on the client side
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA_e3ZDRKdUFhcuaQ41Jdnboi7ew5nZRs0",
+    authDomain: "simple-firebase-18c20.firebaseapp.com",
+    projectId: "simple-firebase-18c20",
+    storageBucket: "simple-firebase-18c20.firebasestorage.app",
+    messagingSenderId: "168038809560",
+    appId: "1:168038809560:web:ea738371c54b6f77097cad"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
