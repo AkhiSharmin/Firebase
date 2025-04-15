9e443af6d5ad4e63ab6866e6020d0247d0945ee7
AkhiSharmin <akhisharmin100@gmail.com>
2025-04-16 00:04:54 +0600

    added google login system

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,21 +1,33 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../../firebase/firebase.init";
+import { useState } from "react";
 
 const Login = () => {
+  const [user, setUser] = useState(null);
+
   const provider = new GoogleAuthProvider();
   const handleGoogleSingIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
-        console.log(result);
+        console.log(result.user);
+        setUser(result.user);
       })
       .catch((error) => {
         console.log("Error", error);
+        setUser(null);
       });
   };
 
   return (
     <div>
       <button onClick={handleGoogleSingIn}>Login with Google</button>
+      {user && (
+        <div>
+          <img src={user.photoURL} alt="" />
+          <h4>{user.displayName}</h4>
+          <p>{user.email}</p>
+        </div>
+      )}
     </div>
   );
 };
